c28d545e71c64611ef3ead75df6aaf79feed469f
tonymbarton <36773585+tonymbarton@users.noreply.github.com>
2019-04-17 09:57:52 -0400

    Add files via upload

---
+++ changeprimarygroup_removedomainuser.ps1
@@ -0,0 +1,16 @@
+﻿$OUpath = 'OU=Extranet,DC=wagmanblue,DC=com'
+$users = Get-ADUser -Filter * -SearchBase $OUpath -Properties SamAccountName
+Foreach ($user in $users) {
+    
+    Add-ADGroupMember "s_noaccess" -Members $user.samaccountname
+
+    $group = Get-ADGroup "s_noaccess"
+    $groupsid = $group.sid
+    $PrimaryGroupID = $groupsid.Value.Substring($groupsid.Value.LastIndexOf('-')+1)
+
+    Set-ADUser -Identity $user.samaccountname -replace @{PrimaryGroupID=$PrimaryGroupID}
+
+    Remove-ADGroupMember "Domain Users" -confirm:$false -member $user.samaccountname
+
+    Write-Host -ForegroundColor Green "Complete."
+    }
